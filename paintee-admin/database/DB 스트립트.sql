8c52c819a2b13b08291cb0a03120302f708eaa97
javadori <javahaja@hotmail.com>
2016-03-02 01:49:32 +0900

    file upload 관련 공통 모듈 작업 cors 를위한 spring 버젼 업데이트

--- paintee-admin/database/DB 스트립트.sql
+++ paintee-admin/database/DB 스트립트.sql
@@ -13,7 +13,8 @@ create table TB_USER (
 	resent_send_addr varchar(200) comment '최근 보낸 주소',
 	resent_send_name varchar(30) comment '최근 보낸 이름',
 	point int comment '엽서를 구매할 수 있는 포인트',
-	user_status char(1) default 'N' comment '계정 상태(정상-N/정지-S/휴먼-Q)'
+	user_status char(1) default 'N' comment '계정 상태(정상-N/정지-S/휴먼-Q)',
+    created_date datetime default now() comment '생성일시'
 );
 
 ALTER TABLE TB_USER COMMENT '회원';
@@ -21,6 +22,7 @@ ALTER TABLE TB_USER COMMENT '회원';
 create table TB_FOLLOW (
 	user_id varchar(30) not null comment 'Follow를 하는 사람 ID (follower)',
 	following varchar(30) not null comment 'Follow되는 대상 ID',
+    created_date datetime default now() comment '생성일시',
 	primary key (user_id, following)
 );
 
@@ -38,7 +40,8 @@ create  table TB_PURCHASE (
 	receiver_name varchar(200) comment '엽서 수신자 이름',
 	sender_name varchar(30) comment '엽서 발신자 이름',
 	location varchar(40) comment '구매자의 국가',
-	purchase_status char(1) comment '구매 진행 상태 (요청-R/발송-S/환불요청-R/삭제-D)'
+	purchase_status char(1) comment '구매 진행 상태 (요청-R/발송-S/환불요청-R/삭제-D)',
+    created_date datetime default now() comment '생성일시'
 );
 
 ALTER TABLE TB_PURCHASE COMMENT '구매';
@@ -49,7 +52,8 @@ create table TB_REWARD (
     account_no varchar(20) comment '입금 요청된 계좌번호',
     account_name varchar(30) comment '입금 요청된 계좌주 이름',
     earm_requested_money int comment '입금 요청된 금액',
-    reward_status char(1) comment '입금 진행 상태 (요청-R/비정상-A/완료-C)'
+    reward_status char(1) comment '입금 진행 상태 (요청-R/비정상-A/완료-C)',
+    created_date datetime default now() comment '생성일시'
 );
 
 ALTER TABLE TB_REWARD COMMENT '리워드';
@@ -66,7 +70,9 @@ create table TB_PAINTING (
     original_size varchar(10) comment '원본 이미지의 size',
     view_cnt int comment '조회수',
     share_cnt int comment 'share 버튼을 통해 공유된 횟수',
-    painting_status char(1) comment '현재 그림의 상태(정상-N/블라인드-B/삭제-D)'
+    file_group_seq bigint comment '첨부파일 그룹 아이디',
+    painting_status char(1) comment '현재 그림의 상태(정상-N/블라인드-B/삭제-D)',
+    created_date datetime default now() comment '생성일시'
 );
 
 ALTER TABLE TB_PAINTING COMMENT '페인팅';
@@ -95,4 +101,28 @@ create table TB_LOGIN (
 
 ALTER TABLE TB_LOGIN COMMENT '로그인';
 
+create table TB_FILE_GROUP (
+    seq bigint not null auto_increment comment '파일그룹아이디',
+    group_name varchar(255) comment '그룹명',
+    created_date datetime default now() comment '생성일자',
+    primary key (seq)
+);
 
+ALTER TABLE TB_FILE_GROUP COMMENT '파일 그룹';
+
+create table TB_FILE_INFO (
+    id varchar(255) not null comment '파일 아이디',
+    file_group_seq bigint not null comment '파일그룹아이디',
+    content_type varchar(255) comment '파일 mime type',
+    extension varchar(255) comment '파일 확장자',
+    name varchar(255) comment '서버저장 파일명(확장자 제외)',
+    ori_name varchar(255) comment '사용자가 업로드한 파일명(확장자 포함)',
+    display_name varchar(255) comment '화면표시용 파일명',
+    path varchar(255) comment '파일명과 절대경로를 제외한 파일 경로',
+    size bigint comment '파일 용량',
+    created_date datetime default now() comment '생성일자',
+    last_modified_date datetime default now() comment '수정일자',
+    primary key (id)
+);
+
+ALTER TABLE TB_FILE_INFO COMMENT '파일 정보';
