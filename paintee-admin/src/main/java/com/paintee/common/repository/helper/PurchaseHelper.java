79afaec1aafd3ac5fccdc4ce8a721a7e4f92fdf9
sonbyungchan <sbc2000kr@naver.com>
2016-03-05 15:45:06 +0900

    인기 그림 헬퍼

---
+++ paintee-admin/src/main/java/com/paintee/common/repository/helper/PurchaseHelper.java
@@ -0,0 +1,37 @@
+/**
+@file PurchaseHelper.java
+@section 파일생성정보
+|    항  목       |      내  용       |
+| :-------------: | -------------   |
+| File name | PurchaseHelper.java |    
+| Package | com.paintee.common.repository.helper |    
+| Project name | paintee-admin |    
+| Type name | PurchaseHelper |    
+| Company | Paintee | 
+| Create Date | 2016 2016. 3. 3. 오후 11:09:41 |
+| Author | Administrator |
+| File Version | v1.0 |
+*/
+package com.paintee.common.repository.helper;
+
+import com.paintee.common.repository.mapper.PurchaseMapper;
+
+/**
+@class PurchaseHelper
+com.paintee.common.repository.helper \n
+   ㄴ PurchaseHelper.java
+ @section 클래스작성정보
+    |    항  목       |      내  용       |
+    | :-------------: | -------------   |
+    | Company | Paintee |
+    | Author | Administrator |
+    | Date | 2016. 3. 3. 오후 11:09:41 |
+    | Class Version | v1.0 |
+    | 작업자 | Administrator |
+ @section 상세설명
+ - 상세설명 은 여기에 기입해 주세요.
+ -# 여기는 리스트로 표시됩니다.
+*/
+public interface PurchaseHelper extends PurchaseMapper {
+
+}
