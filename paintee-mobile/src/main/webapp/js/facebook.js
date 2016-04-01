d103a92106f91af35608ae24117f03275a13eae2
javadori <javahaja@hotmail.com>
2016-04-01 16:32:54 +0900

    no message

--- paintee-mobile/src/main/webapp/js/facebook.js
+++ paintee-mobile/src/main/webapp/js/facebook.js
@@ -27,7 +27,7 @@ function statusChangeCallback(response) {
 
 window.fbAsyncInit = function() {
 	FB.init({
-		appId      : '965948916784381',
+		appId      : '235488130138448',
 		cookie     : true,  // enable cookies to allow the server to access 
 		// the session
 		xfbml      : true,  // parse social plugins on this page
