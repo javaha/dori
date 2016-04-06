0df55f1ad9756da6c22a9dd2c28c808f6af392f6
javadori <javahaja@hotmail.com>
2016-04-06 22:34:44 +0900

    서버에서 알수없는 오류 발생시 alert 발생후 메인페이지로 이동 추가

--- paintee-mobile/src/main/webapp/js/util/common-ajax.js
+++ paintee-mobile/src/main/webapp/js/util/common-ajax.js
@@ -17,63 +17,8 @@ var AjaxCall = {
 				}
 			},
 			error: function (jqXHR, textStatus, errorThrown) {
-				// console.log(jqXHR);
-				// console.log(jqXHR.responseText);
-				// console.log(textStatus);
-				// console.log(errorThrown);
-
-				if (jqXHR.responseText != null) {
-					try {
-						var object = JSON.parse(jqXHR.responseText);
-						var contextPath = object.contextPath?object.contextPath:'';
-
-						if (opener == null) {
-							if (object.errorNo == 500) {
-								var url = contextPath + "/common/error/500.html";
-
-								$(location).attr('href', url);
-							} else if (object.errorNo == 9999) {
-								clearUserInfoCookie();
-
-								alert(object.errorMsg);
-
-								$(location).attr('href', contextPath + '/');
-							} else {
-								$(location).attr('href', object.redirectUrl);
-							}
-						} else {
-							if (object.errorNo == 500) {
-								var url = object.contextPath + "/common/error/500.html";
-
-								$(opener.location).attr('href', url);
-							} else if (object.errorNo == 9999) {
-								clearUserInfoCookie();
-
-								alert(object.errorMsg);
-
-								$(location).attr('href', contextPath + '/');
-							} else {
-								$(opener.location).attr('href', object.redirectUrl);
-							}
-
-							self.close();
-						}
-					} catch (e) {
-						alert(e.message);
-						alert(jqXHR.responseText);
-						alert('response not null == Error1 == \n' +
-							'code : ' + jqXHR.status + '\n' +
-							'statusText : ' + jqXHR.statusText
-						);
-					}
-				} else {
-					alert('=== Error2 === \n' +
-						'code : ' + jqXHR.status + '\n' +
-						'statusText : ' + jqXHR.statusText
-					);
-				}
-
-				return;
+				alert($.i18n.t('alert.common.unkownServerError'));
+				location.href = "/";
 			}
 		};
 
@@ -99,45 +44,7 @@ var AjaxCall = {
 				successFunc(result, status);
 			},
 			error: function (jqXHR, textStatus, errorThrown) {
-				//alert(jqXHR.responseText);
-				if (jqXHR.responseText != null) {
-					try {
-						var object = JSON.parse(jqXHR.responseText);
-						var contextPath = object.contextPath?object.contextPath:'';
-
-						if (opener == null) {
-							if (object.errorNo == 500) {
-								var url = contextPath + "/common/error/500.html";
-
-								$(location).attr('href', url);
-							} else {
-								$(location).attr('href', object.redirectUrl);
-							}
-						} else {
-							if (object.errorNo == 500) {
-								var url = contextPath + "/common/error/500.html";
-
-								$(opener.location).attr('href', url);
-							} else {
-								$(opener.location).attr('href', object.redirectUrl);
-							}
-
-							self.close();
-						}
-					} catch (e) {
-						alert('== Error == \n' +
-							'code : ' + jqXHR.status + '\n' +
-							'statusText : ' + jqXHR.statusText
-						);
-					}
-				} else {
-					alert('=== Error === \n' +
-						'code : ' + jqXHR.status + '\n' +
-						'statusText : ' + jqXHR.statusText
-					);
-				}
-
-				return;
+				location.href = "/";
 			}
 		});
 	}
