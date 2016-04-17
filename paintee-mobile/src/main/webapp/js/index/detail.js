b9088f8690240590e4426c94df8cf09ffe462c4f
javadori <javahaja@hotmail.com>
2016-04-17 22:04:56 +0900

    no message

--- paintee-mobile/src/main/webapp/js/index/detail.js
+++ paintee-mobile/src/main/webapp/js/index/detail.js
@@ -407,7 +407,7 @@ PostedController.prototype = {
 				purchaseSeq: "",
 				userId: "",
 				userName: "",
-				sentence: "데이터가 존재하지 않습니다."
+				sentence: $.i18n.t('detailPop.noData')
 			};
 			addPosted(this.swiper, postedInfo);
 		} else {
