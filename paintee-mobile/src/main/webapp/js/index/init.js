fc6e86ea3a0d0ab03380f5db3503dfc41b7c5776
Ernest Hwang <Ernest Hwang>
2016-04-27 21:16:46 +0900

    side 메뉴에 email대신 username 표시

--- paintee-mobile/src/main/webapp/js/index/init.js
+++ paintee-mobile/src/main/webapp/js/index/init.js
@@ -459,11 +459,9 @@ function initMenu(userID){
         sideLogin.append($("<div>").addClass("login_btn").html("Log in").css("border-color", "rgb(100,100,100)").click(function(){showLogin()}));
     }else{
         sideLogin.empty()
-        sideLogin.append($("<div>").addClass("side_menu_login_id").html(userID)).on("click", function(){showProfile()});
+        sideLogin.append($("<div>").addClass("side_menu_login_id").html(userInfo.name)).on("click", function(){showProfile()});
 
-        var editBtn = $("<a>").html("edit");
-        var logoutBtn = $("<a>").html("logout");
-        var btnGroup = $("<div>").append(editBtn).append(" | ").append(logoutBtn).on("click", function(){showProfile()});
+        var editBtn = $("<a>").html(userID).on("click", function(){showProfile()});
 
         sideLogin.append(btnGroup);
     }
